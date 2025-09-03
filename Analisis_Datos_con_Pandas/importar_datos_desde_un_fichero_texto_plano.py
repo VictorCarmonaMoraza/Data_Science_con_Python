c48a16748c2fcdac0a5135ff6933c6fb4c856e28
Victor Carmona <victorcarmona9035.com>
2025-09-03 13:22:40 +0200

    Como importar datos desde un fichero de texto plano (txt,csv,...) version nueva

--- Analisis_Datos_con_Pandas/importar_datos_desde_un_fichero_texto_plano.py
+++ Analisis_Datos_con_Pandas/importar_datos_desde_un_fichero_texto_plano.py
@@ -1,12 +1,13 @@
 ## EJERCICIO
-## Cargar los primeros 10.000 registros del fichero CSV de trafico
+## Cargar los primeros 535 registros del fichero CSV de trafico
 ## de peatones (delimitador ";") indicando como indice del Dataframe
-## la columna "IDENTIFICADOR"
+## la columna "CÓDIGO_POSTAL"
 
 import pandas as pd
 
 
-df = pd.read_csv(r"PEATONES_2020.csv", delimiter=";", nrows=10000, index_col="IDENTIFICADOR", encoding="ISO-8859-1")
+df = pd.read_csv(r"pedestrian_information_2020.csv", delimiter=";",
+                 nrows=535, index_col="CÓDIGO_POSTAL", encoding="ISO-8859-1")
 
 print(df.head())
 print(df.info())
